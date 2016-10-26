53fff3fe3b5126ebe236d5e9f0a5c67da471c703
Jacob Joaquin <jacobjoaquin@gmail.com>
2016-10-26 12:40:00 -0700

    Updated main with simple point rectangle collision

--- main.py
+++ main.py
@@ -8,10 +8,23 @@ height = 500
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Old School Video Game')
 
+background_color = (180, 180, 180)
+
+r1 = pygame.Rect(200, 100, 250, 50)
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.fill(background_color)
+
+    c = (0, 0, 255)
+
+    if r1.collidepoint(pygame.mouse.get_pos()):
+        c = (255, 0, 0)
+
+    pygame.draw.rect(screen, c, r1)
 
     pygame.display.update()
